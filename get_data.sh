fc97ed5ee680bff8cf6c3c5d9b3189e22a5bd975
mesahwi <33714067+mesahwi@users.noreply.github.com>
2021-06-08 02:11:45 +0900

    Delete get_data.sh

--- get_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget --load-cookies ~/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies ~/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1TbuQttfgIiSXHe6i0ihyl3N4cesnH1Eo' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1TbuQttfgIiSXHe6i0ihyl3N4cesnH1Eo" -O Data.zip && rm -rf ~/cookies.txt
-unzip Data.zip
-mkdir KNN_results
