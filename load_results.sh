27d37cf324518d123b30ebcc92fe55e5ba675cb9
mesahwi <33714067+mesahwi@users.noreply.github.com>
2021-06-08 01:25:23 +0900

    Delete load_results.sh

--- load_results.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget --load-cookies ~/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies ~/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1fyxnsQKH34uyvpr89fC3CG6ac1mT4fKg' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1fyxnsQKH34uyvpr89fC3CG6ac1mT4fKg" -O KNN_results.zip && rm -rf ~/cookies.txt
-mkdir KNN_results
-unzip KNN_results.zip -d KNN_results
